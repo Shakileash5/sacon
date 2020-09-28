a7f0bd89c8418de0933c263b9c3bfcbcc2e9bd70
SHAKILEASH <shakileash2000@gmail.com>
2020-09-28 23:58:12 +0530

    handled data fetch

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import os
+import json
 import pyrebase
 
 app = Flask(__name__)
@@ -16,9 +17,12 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 value = dict(db.child("thesis_data").get().val())
 keys = value.keys()
-print(keys)
+
 @app.route("/")
 def index():
+    global keys,value   
+    value = dict(db.child("thesis_data").get().val())
+    keys = value.keys()
     return render_template("index.html")
 
 @app.route("/search",methods = ["GET","POST"])
@@ -26,24 +30,43 @@ def search():
     if request.method == "POST":
         data = dict(request.form)
         find = data["name"]
-        print(find)
-        
-    for key in keys:
-        gets = list(value[key].values())
-        #print(gets)
-        for get in gets:
-            if isinstance(get, str) :
-                if find in get:
-                    print(find)
-            if isinstance(get, list):
-                for l in get:
-                    #print(l)
-                    if find in l:
-                        print(True)
-            if isinstance(get, int):
-                if find ==  get:
-                    print(find)
-    return "200"
+        #print(find)
+    try:
+        find_key = []   
+        for key in keys:
+            gets = list(value[key].values())
+            find = find.lower()
+            for get in gets:
+                if isinstance(get, str) :
+                    get = get.lower()
+                    if find in get:
+                        print(find,get)
+                        if key not in find_key:
+                            find_key.append(key)
+                if isinstance(get, list):
+                    for l in get:
+                        l = l.lower()
+                        if find in l:
+                            if key not in find_key:
+                                find_key.append(key)
+                if isinstance(get, int):
+                    if find in str(get):
+                        if key not in find_key:
+                            find_key.append(key)
+
+        print("Matched data's are:::",find_key)
+        data = {}
+        i = 0
+        for key in find_key:
+            data[str(i)] = [value[key]["Author"],value[key]["Title"],value[key]["Type"],value[key]["Year"],value[key]["Label"]]
+            i = i+1
+        data = json.dumps(data)    
+        print("Matched data's are:::",data)    
+        return data
+    except:
+            return "500"
+                    
+    
 
 
 if __name__ == "__main__":
