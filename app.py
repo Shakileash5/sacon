720b6612a9b799c960ede7c57923bcb50e686ceb
SHAKILEASH <shakileash2000@gmail.com>
2020-09-28 12:55:36 +0530

    handeled ajax

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/search",methods = ["GET","POST"])
+def search():
+    if request.method == "POST":
+        data = dict(request.form)
+        print(data)
+    return "200"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)),debug=True,use_reloader=True)
